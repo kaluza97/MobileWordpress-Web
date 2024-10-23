184c203c3dcbdf61b7efe18581f0bb9a62419e39
pkaluzny <piotr.kaluzny@capgemini.com>
2024-10-23 14:56:06 +0200

    add error message and type for dnd elements

--- src/components/SettingsMenu/BottomNavigationSettings/BottomNavigationSettings.styles.ts
+++ src/components/SettingsMenu/BottomNavigationSettings/BottomNavigationSettings.styles.ts
@@ -1,14 +1,26 @@
-import { SxProps } from "@mui/material";
+import { Colors } from '@/variables/Colors';
+import { SxProps } from '@mui/material';
 
 export const container: SxProps = {
-    display: "flex",
-    flexDirection: 'column',
+  display: 'flex',
+  flexDirection: 'column',
 };
 
 export const selectContainer: SxProps = {
-    marginTop: '2rem',
-    display: 'flex',
-    justifyContent: 'center',
-
+  marginTop: '2rem',
+  display: 'flex',
+  justifyContent: 'center',
 };
 
+export const sectionText: SxProps = {
+  marginBottom: '1rem',
+  fontSize: '1.6rem',
+  fontWeight: 'bold',
+};
+
+export const checkButtonStyles: SxProps = {
+  marginTop: '2rem',
+  marginBottom: '4rem',
+  backgroundColor: Colors.green,
+  fontWeight: 'bold',
+};
--- src/components/SettingsMenu/BottomNavigationSettings/BottomNavigationSettings.tsx
+++ src/components/SettingsMenu/BottomNavigationSettings/BottomNavigationSettings.tsx
@@ -1,15 +1,43 @@
-"use client";
+'use client';
+import CheckIcon from '@mui/icons-material/Check';
+
 import { Box, Button, Typography } from '@mui/material';
-import { container } from '@/components/SettingsMenu/BottomNavigationSettings/BottomNavigationSettings.styles';
+import {
+  checkButtonStyles,
+  container,
+  sectionText,
+} from '@/components/SettingsMenu/BottomNavigationSettings/BottomNavigationSettings.styles';
 import { SettingsForm } from './SettingsForm/SettingsForm';
 
-export const BottomNavigationSettings = () => {
+interface BottomNavigationSettingsProps {
+  SettingsSections: Array<{ sectionId: string }>;
+}
 
-    return (
-        <Box sx={container}>
-            <Button>CONFIRM</Button>
-            <Typography>1/5</Typography>
-            <SettingsForm />
+const handleConfirm = () => {
+  console.log('ZATWIERDZONO');
+};
+
+export const BottomNavigationSettings = ({
+  SettingsSections,
+}: BottomNavigationSettingsProps) => {
+  return (
+    <Box sx={container}>
+      {SettingsSections.map(({ sectionId }) => (
+        <Box key={sectionId}>
+          <Typography
+            sx={sectionText}
+          >{`${sectionId}/${SettingsSections.length}`}</Typography>
+          <SettingsForm />
+          <Button
+            variant="contained"
+            startIcon={<CheckIcon />}
+            onClick={handleConfirm}
+            sx={checkButtonStyles}
+          >
+            Confirm
+          </Button>
         </Box>
-    );
+      ))}
+    </Box>
+  );
 };
